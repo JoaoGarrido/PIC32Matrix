768d154d6e4b5d4c3f8ff170e3543357d9387c55
Taillow <my.blog.jp@gmail.com>
2019-01-08 19:39:57 +0000

    Basic logic implementation and some testing

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,22 @@
 #define C RD11
 #define D RD5
 
+//Modify Output
 #define latA LATDbits.LATD7
 #define latB LATDbits.LATD6
 #define latC LATDbits.LATD11
 #define latD LATDbits.LATD5
+#define latLAT LATFbits.LATF6
+#define lat_OE LATFbits.LATF4 //_ inverted Output
+#define latR1 LATEbits.LATE7 
+#define latG1 LATEbits.LATE6 
+#define latB1 LATEbits.LATE5
+#define latR2 LATEbits.LATE4
+#define latG2 LATEbits.LATE3
+#define latB2 LATEbits.LATE2
+#define latCLK LATEbits.LATE1
+
+#define nLedsLine 192
 
 void initMATRIX(){
   //Set 0utputs
@@ -32,32 +44,47 @@ void initMATRIX(){
   LATE = 0;
   LATF = 0;
   LATD = 0;
-  LATFbits.LATF4 = 1; //turn off output enable
+  lat_OE = 1; //turn off output enable
+}
+
+void latch(){
+  latLAT = 1;
+  latLAT = 0;
+  lat_OE = 0;
+  delay(1); //NEEDS DELAY for propagation! 
+  lat_OE = 1;
 }
 
 void selectRow(uint8_t row){
-  //LAT up and OE
-  LATFbits.LATF6 = 1;
-  LATFbits.LATF4 = 1;
+  latA = (row & 0x01);
+  latB = (row & 0x02) >> 1;
+  latC = (row & 0x04) >> 2;
+  latD = (row & 0x08) >> 3;
 
-  latA = (row & 0x08) >> 3;
-  latB = (row & 0x04) >> 2;
-  latC = (row & 0x02) >> 1;
-  latD = (row & 0x01);
   //Send color information
+  for(int a = 0; a < nLedsLine; a++){
+    latCLK = 1;
+    latCLK = 0;
+  }
+  latch();
 
-
-  LATFbits.LATF6 = 1;
-  LATFbits.LATF4 = 0;
 }
 
 void setup() {
   initMATRIX();
+  latR1 = 1;
+  latG1 = 1;
+  latB1 = 1;
+  latR2 = 1;
+  latG2 = 1;
+  latB2 = 1;
 }
 
 void loop() {
-  LATEbits.LATE1 = 1;
-  selectRow(1);
-  delay(1);
-  LATEbits.LATE1 = 0;
+  static int i = -1;
+  //delay(1);
+  if(i < 15) i++;
+  else i = 0;
+  selectRow(i);
+  //lat_OE = 0;
 }
